e3bf016cdf040c796ce0b382fd8fd2e9b0029cd8
Sandoval <sarasandoval1018@gmail.com>
2023-06-14 11:53:08 -0500

    Error component changes

--- finalProject/src/components/Dashboard.jsx
+++ finalProject/src/components/Dashboard.jsx
@@ -61,7 +61,46 @@ const Dashboard = () => {
   }
 
   if (error) {
-    return <p>Error: {error.message}</p>;
+    return (
+      <div className="error">
+        <p>Error: {error.message}</p>
+        <div className="gearbox">
+          <div className="overlay"></div>
+          <div className="gear one">
+            <div className="gear-inner">
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+            </div>
+          </div>
+          <div className="gear two">
+            <div className="gear-inner">
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+            </div>
+          </div>
+          <div className="gear three">
+            <div className="gear-inner">
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+            </div>
+          </div>
+          <div className="gear four large">
+            <div className="gear-inner">
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+
   }
 
   return (
